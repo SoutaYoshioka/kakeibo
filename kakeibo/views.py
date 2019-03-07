f8e1e2e5ab12d31200614316244cf4cce8487dc9
Martinnnnn <martinnnnn.114514@gmail.com>
2019-03-07 13:28:51 +0900

    add view

--- kakeibo/views.py
+++ kakeibo/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from .models import Category, Kakeibo
+from .forms import KakeiboForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class KakeiboListView(ListView):
+
+    model = Kakeibo
+
+
+class KakeiboCreateView(CreateView):
+
+    model = Kakeibo
+    form_class = KakeiboForm
+    success_url = reverse_lazy('kakeibo:create_done')
+
+
+def create_done(request):
+
+    return render(request, 'kakeibo/create_done.html')
+
+
+class KakeiboUpdateView(UpdateView):
+
+    model = Kakeibo
+    form_class = KakeiboForm
+    success_url = reverse_lazy('kakeibo:update_done')
+
+
+def update_done(request):
+
+    return render(request, 'kakeibo/update_done.html')
