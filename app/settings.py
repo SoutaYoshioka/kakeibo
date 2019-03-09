51fddb3a02905562abf9065cf53432ee3806efbe
Martinnnnn <martinnnnn.114514@gmail.com>
2019-03-09 15:00:57 +0900

    heroku~~~~

--- app/settings.py
+++ app/settings.py
@@ -116,15 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 DEBUG = False
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
-if not DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
